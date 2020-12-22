01f02020e4583379bd91d89ecbd32c08453985b0
JúDCraide <juliadcraide31@gmail.com>
2020-12-22 01:17:06 -0300

    Autenticação

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '6aa2e5edd9c9df1be6ccdc119e3c6e7e',
+    expiresIn: '1d',
+  },
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
-import userRouter from './users.routes';
 import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
+import sessionsRouter from './sessions.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
-routes.use('/users', userRouter);
+routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
