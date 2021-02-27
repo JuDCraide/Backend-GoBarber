f0dfe198d0beeddb0a796fcbd2f9782864310c48
JúDCraide <juliadcraide31@gmail.com>
2021-02-27 01:42:44 -0300

    Cache lista de providers

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -3,17 +3,32 @@ import { inject, injectable } from 'tsyringe';
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+
 @injectable()
 class ListProvidersService {
   constructor(
     @inject('UsersRepository')
     private UsersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) { }
 
   public async run(
     user_id: string,
   ): Promise<User[]> {
-    const users = await this.UsersRepository.listProviders(user_id);
+    let users = await this.cacheProvider.recover<User[]>(`providers-list:${user_id}`);
+
+    if (!users) {
+      users = await this.UsersRepository.listProviders(user_id);
+      console.log('Query foi feita');
+
+      await this.cacheProvider.save(
+        `providers-list:${user_id}`,
+        users,
+      );
+    }
 
     return users;
   }
